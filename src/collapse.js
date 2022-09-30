64a4e3d8af204edfcc8603a7fa2d07cf4ed55cb9
zhao-yiming <zhao.yiming423@gmail.com>
2022-10-01 00:26:13 +0200

    delete file "IOT_interface"

---
+++ src/collapse.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import useCollapse from 'react-collapsed';
+import './App.css'
+
+
+function Expand (props) {
+    const config = {
+      defaultExpanded: props.defaultExpanded || false,
+      collapsedHeight: props.collapsedHeight || 0
+    };
+    const { getCollapseProps, getToggleProps, isExpanded } = useCollapse(config);
+
+    return (
+      <div className="collapsible">
+      <div className="header" {...getToggleProps()}>
+          <div className="title">{props.title}</div>
+          <div className="icon">
+              <i className={'fas fa-chevron-circle-' + (isExpanded ? 'up' : 'down')}></i>
+          </div>
+      </div>
+      <div {...getCollapseProps()}>
+          <div className="content">
+              {props.children}
+          </div>
+      </div>
+  </div>
+    );
+  }
+
+
+
+
+
+
+export default Expand;
