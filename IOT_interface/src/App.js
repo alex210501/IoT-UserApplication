5497aba37e606208e6aac3b3db4a6ed8cc895f01
zhao-yiming <zhao.yiming423@gmail.com>
2022-11-14 09:44:33 +0100

    delete device1 and device2

--- IOT_interface/src/App.js
+++ IOT_interface/src/App.js
@@ -8,22 +8,15 @@ function App() {
   const [ans, setans] = useState([]);
  
 
-  const [temp_min, set_temp_min] = useState(15);
+  const [temp_min, set_temp_min] = useState(20);
   const [temp_max, set_temp_max] = useState(25);
 
-  const [humidity_min, set_humidity_min] = useState(60);
-  const [humidity_max, set_humidity_max] = useState(80);
-
-  const [co2_min, set_co2_min] = useState(0);
-  const [co2_max, set_co2_max] = useState(50);
+  const [humidity_min, set_humidity_min] = useState(40);
+  const [humidity_max, set_humidity_max] = useState(50);
   
   const [temperature, set_temperature] = useState(0);
   const [humidity, set_humidity] = useState(0);
-  const [co2, set_co2 ]= useState(0);
   const [location, set_location ]= useState(0);
-
-  
-
 
 
   const change_temp_max =(event)=>{
@@ -38,21 +31,16 @@ function App() {
   const change_humidity_min =(event)=>{
     set_humidity_min(event.target.valueAsNumber);
   }
-  const change_co2_max =(event)=>{
-    set_co2_max(event.target.valueAsNumber);
-  }
-  const change_co2_min =(event)=>{
-    set_co2_min(event.target.valueAsNumber);
-  }
+ 
   const MINUTE_MS = 1000;
   useEffect(() => {
     const interval = setInterval(() => {
-      fetch('http://172.17.36.72:8080/')
+      fetch('http://172.17.37.75:5001/')
      .then(response=>response.json())
       .then(data=>{setans(data);
       set_temperature(data[data.length-1].data.temperature);
       set_humidity(data[data.length-1].data.humidity);
-      //set_co2(data[data.length-1].data.co2);
+
       set_location(data[data.length-1].data.gps);
     })
     }, MINUTE_MS);
@@ -67,24 +55,12 @@ function App() {
       <div className="Gateway">
       <div className="Gateway1" >
       <Expand title="Gateway 1" defaultExpanded= {true} >
-          <h3>Device 1</h3>
-          <label className="capteur"><div className="data" style={{ backgroundColor: temperature > temp_max  | temperature<temp_min ? 'red':'green'}}> </div> Température: {temperature}°C</label><br/>
-          <label className="capteur"><div className="data" style={{ backgroundColor: humidity > humidity_max  | humidity<humidity_min ? 'red':'green'}}> </div> Humidity: {humidity}</label ><br/>
-          <label className="capteur"><div className="data" style={{ backgroundColor: co2 > co2_max  | co2<co2_min ? 'red':'green'}}> </div> CO2: {co2}</label ><br/>
-          <label className="capteur"><div className="data" style={{ backgroundColor: 'white'}}> </div> GPS: {location.latitude + ' , '+location.longitude} <a  href={ "https://www.google.com/maps/search/?api=1&query=" +location.latitude+','+location.longitude} target="_blank" >Google Maps</a></label ><br/>
-          
           <h3>Device 2</h3>
           <label className="capteur"><div className="data" style={{ backgroundColor: temperature > temp_max  | temperature<temp_min ? 'red':'green'}}> </div> Température: {temperature}°C</label><br/>
-          <label className="capteur"><div className="data" style={{ backgroundColor: humidity > humidity_max  | humidity<humidity_min ? 'red':'green'}}> </div> Humidity: {humidity}</label ><br/>
-          <label className="capteur"><div className="data" style={{ backgroundColor: co2 > co2_max  | co2<co2_min ? 'red':'green'}}> </div> CO2: {co2}</label ><br/>
-          <label className="capteur"><div className="data" style={{ backgroundColor: 'white'}}> </div> GPS</label ><br/>
-
-          <h3>Device 3</h3>
-          <label className="capteur"><div className="data" style={{ backgroundColor: temperature> temp_max  | temperature<temp_min ? 'red':'green'}}> </div> Température: {temperature}°C</label><br/>
-          <label className="capteur"><div className="data" style={{ backgroundColor: humidity > humidity_max  | humidity<humidity_min ? 'red':'green'}}> </div> Humidity: {humidity}</label ><br/>
-          <label className="capteur"><div className="data" style={{ backgroundColor: co2 > co2_max  | co2<co2_min ? 'red':'green'}}> </div> CO2: {co2}</label ><br/>
-          <label className="capteur"><div className="data" style={{ backgroundColor: 'white'}}> </div> GPS</label ><br/>
-         
+          <label className="capteur"><div className="data" style={{ backgroundColor: humidity > humidity_max  | humidity<humidity_min ? 'red':'green'}}> </div> Humidity: {humidity}%</label ><br/>
+          <label className="capteur"><div className="data" style={{ backgroundColor: 'white'}}> </div> GPS: {location.latitude + ' , '+location.longitude} <a  href={ "https://www.google.com/maps/search/?api=1&query=" +location.latitude+','+location.longitude} target="_blank" >Google Maps</a></label ><br/>
+          
+        
       </Expand>
       </div>
 
@@ -104,9 +80,9 @@ function App() {
             <div className='temperature'> 
               <h3>Temperature</h3>
               <div className='temperature_field'>
-                    Min: <input type="number" className='temperature_input' defaultValue={90} min={0} onChange={change_temp_min}/>
+                    Min: <input type="number" className='temperature_input' defaultValue={temp_min} min={0} onChange={change_temp_min}/>
                     <br/><br/>
-                    Max: <input type="number" className='temperature_input' defaultValue={100} onChange={change_temp_max}/>
+                    Max: <input type="number" className='temperature_input' defaultValue={temp_max} onChange={change_temp_max}/>
                     <br/><br/>
               </div>
             </div>
@@ -114,9 +90,9 @@ function App() {
             <div className='Humidity'> 
               <h3>Humidity</h3>
               <div className='Humidity_field'>
-                    Min: <input type="number" className='Humidity_input' defaultValue={90} min={0} onChange={change_humidity_min}/>
+                    Min: <input type="number" className='Humidity_input' defaultValue={humidity_min} min={0} onChange={change_humidity_min}/>
                     <br/><br/>
-                    Max: <input type="number" className='Humidity_input' defaultValue={100} onChange={change_humidity_max}/>
+                    Max: <input type="number" className='Humidity_input' defaultValue={humidity_max} onChange={change_humidity_max}/>
                     <br/><br/>
               </div>
             </div>
@@ -124,9 +100,9 @@ function App() {
             <div className='CO2'> 
               <h3 className='CO2_text'>CO2</h3>
               <div className='CO2_field'>
-                    Min: <input type="number" className='CO2_input' defaultValue={90} min={0} onChange={change_co2_min}/>
+                    Min: <input type="number" className='CO2_input' defaultValue={90} min={0} />
                     <br/><br/>
-                    Max: <input type="number" className='CO2_input' defaultValue={100} onChange={change_co2_max}/>
+                    Max: <input type="number" className='CO2_input' defaultValue={100} />
                     <br/><br/>
               </div>
             </div>
